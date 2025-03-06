4508a2a1af5ba41e39012469dbd046ac3eaa40ea
starksimaging <jonstarks6@gmail.com>
2025-03-06 18:02:16 -0500

    Enhance checkout form with credit card validation and dynamic input masking; update template for improved user experience

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import MenuItem
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
 
 class MenuItemForm(forms.ModelForm):
     class Meta:
@@ -20,9 +21,46 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     fields = ['username', 'email', 'password', 'password2']
 
-# Credit card form
+# Credit card form using RegexValidator
 class CheckoutForm(forms.Form):
-    card_number = forms.CharField(label='Card Number', max_length=16, min_length=16)
-    expiration_date = forms.CharField(label= 'Expiration Date (MM/YY)', max_length=5)
-    cvv = forms.CharField(label='CVV', max_length=3, min_length=3)
+    card_number = forms.CharField(
+        label='card_number',
+        max_length=25,
+        validators=[
+            RegexValidator(
+                regex=r'^\d{4} \d{4} \d{4} \d{4}|\d{16}$',
+                message='Please enter a valid credit card number'
+            )
+            
+        ]
+    )
+    expiration_date = forms.CharField(
+        label='Expiration Date (mm/yy)',
+        max_length=5,
+        validators=[
+            RegexValidator(
+                regex=r'^(0[1-9]|1[0-2])\/\d{2}$',
+                message='Enter a valid expiration date in MM/YY format',
+            )
+                
+
+                    
+
+        ]
+    )
+    cvv = forms.CharField(
+        label='CVV',
+        max_length=3,
+        validators=[
+            RegexValidator(
+                regex=r'^\d{3,4}$',
+                message='Enter a valid CVV code'
+            )
+        ]
+    )
+
+
+
+
+
     
